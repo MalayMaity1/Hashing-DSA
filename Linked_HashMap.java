2aac0d909a61bd255541bfd3232228e5db4e0681
Malay Maity <maitymalay27747@gmail.com>
2024-11-26 12:28:47 +0530

    LinkedHashMap implementation in JAVA

---
+++ Linked_HashMap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Linked_HashMap{
+    public static void main(String args[]){
+        LinkedHashMap <String ,Integer>lhm=new LinkedHashMap<>();
+        lhm.put("India",1);
+        lhm.put("Nepal",2);
+        lhm.put("Bhutan",3);
+        System.out.println(lhm);
+    }
+}
