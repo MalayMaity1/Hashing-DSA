7a429d70ef2da12171d7d24c3e89ae666b561d33
Malay Maity <maitymalay27747@gmail.com>
2024-11-26 12:41:53 +0530

    LinkedHashSet implementation using JAVA

---
+++ Linked_HashSet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Linked_HashSet{
+    public static void main(String args[]){
+        LinkedHashSet<Integer>set=new LinkedHashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        System.out.println(set);
+    }
+}
