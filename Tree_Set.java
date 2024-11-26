f3c807f71628e35f5c8f47427a340b1103490d98
Malay Maity <maitymalay27747@gmail.com>
2024-11-26 12:44:16 +0530

    TreeMap implementation using JAVA

---
+++ Tree_Set.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Tree_Set{
+    public static void main(String args[]){
+        TreeSet<Integer>set=new TreeSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        System.out.println(set);
+    }
+}
