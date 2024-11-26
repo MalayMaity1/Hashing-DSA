aba706685aadb11fc9d8d5b514fe9137ff9fc4ff
Malay Maity <maitymalay27747@gmail.com>
2024-11-26 12:33:16 +0530

    TreeMap implementation using JAVA.

---
+++ Tree_Map.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Tree_Map{
+    public static void main(String args[]){
+        TreeMap <String ,Integer>tm=new TreeMap<>();
+        tm.put("India",1);
+        tm.put("Nepal",2);
+        tm.put("Bhutan",3);
+        System.out.println(tm);
+    }
+}
