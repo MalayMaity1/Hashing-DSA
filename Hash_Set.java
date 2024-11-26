86c144a8d2736b0a624c2cd51e26fc7fea5ea224
Malay Maity <maitymalay27747@gmail.com>
2024-11-26 12:38:42 +0530

    HashSet implementation using JAVA

---
+++ Hash_Set.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hash_Set{
+    public static void main(String args[]){
+        HashSet<Integer>set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        System.out.println(set);
+    }
+}
