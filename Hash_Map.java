e3fd933c4f2ab42ac510e706ddc49292a4e71c44
Malay Maity <maitymalay27747@gmail.com>
2024-11-26 12:18:49 +0530

    Hashmap implementation  java

---
+++ Hash_Map.java
@@ -0,0 +1,9 @@
+public class Hash_Map{
+    public static void main(String args[]){
+        java.util.HashMap<String,Integer>hm=new java.util.HashMap<>();
+        hm.put("India", 100);
+        hm.put("Nepal",200);
+        hm.put("Bhutan",300);
+        System.out.println(hm);
+    }
+}
